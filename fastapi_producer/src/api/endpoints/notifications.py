45fa03b89d12112e0d5f468a72626ba166f3d333
gio <kge91186@mail.ru>
2025-03-24 15:15:37 +0300

    set to mail

--- fastapi_producer/src/api/endpoints/notifications.py
+++ fastapi_producer/src/api/endpoints/notifications.py
@@ -11,7 +11,7 @@ router = APIRouter()
 async def send_mail(recipient: str = Body(...), 
     subject: str = Body(...), 
     body: str = Body(...),  
-                    rmq_publisher_service:RabbitMQPublisherService=Depends(get_rmq_publisher_service)):
+    rmq_publisher_service:RabbitMQPublisherService=Depends(get_rmq_publisher_service)):
     message = {"recipient":recipient, "subject":subject, "body":body}
     await rmq_publisher_service.send_to_queue(message=message, routing_key="email.send")
     return {"message":"Email queued"}
